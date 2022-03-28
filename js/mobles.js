4c4f68fadcde56a787f06644cf0d538771984780
Antón Vega <anton.paidoslalin@gmail.com>
2022-03-28 13:18:26 +0200

    javacript > javascript index.html

--- js/mobles.js
+++ js/mobles.js
@@ -45,39 +45,49 @@ class mobles{
     set setCor (dato){
         this.cor = dato;
     };
-    //Captura-modificación de Altura en dimensións
-    set setAlto(dato) {
+    //Captura-modificación de alto en dimensións
+        set setAlto(dato) {
         this.dimensions.alto = dato;
     };
+    //Captura-modificacion de ancho en dimensións
     set setAncho(dato) {
         this.dimensions.ancho = dato;
     };
+    //Captura-modificacion de longo en dimensións
     set setLongo(dato) {
         this.dimensions.longo = dato;
     };
+    //Captura-modificación de prezo en dimensións
       set setPrezo(dato){
         this.prezo = dato;
     };
 
     //Getters: Obtención de datos
+    //Obtención de nome
     get getNome(){
         return this.nome;
     }
+    //Obtención de Tipo
     get getTipo(){
         return this.tipo;
     }
+    //Obtención de Cor
     get getCor(){
         return this.Cor;
     }
+    //Obtención de Altura mediante dimensións
     get getAlto(){
-        return this.alto;
+        return this.dimensions.alto;
     }
+    //Obtención de anchura mediante dimensións
     get getAncho(){
         return this.ancho;
     }
+    //Obtención do longo mediante dimensións
     get getLongo(){
         return this.longo;
     }
+    //Obtención do prezo 
     get getPrezo(){
         return this.prezo;
     }
@@ -114,9 +124,14 @@ class mobles{
        this.#numero_patas=nPatas;
     };
 
-    imprimirNumPatas(mob){
+    imprimirMesa(mob){
         super.imprimirMoble(mob);
-
-    };
+        console.log(`======Datos exclusivos Mesa====== \n
+        Número de Patas:    ${this.#numero_patas}`);
+    }
 
  }
+ //Datos de exemplo
+
+ LakkaV55 ={nome:'lakka', tipo:'Mesa', cor:'Verde', dimensions:{alto:0.55,ancho:0.55,alto:0.55},prezo:7.00};
+ imprimirMesa(LakkaV55);
